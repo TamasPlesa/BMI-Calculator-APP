7e98342cb0f68bfcade9fbeb31ce2a533c26f3d0
TamasPlesa <tamasplesa@icloud.com>
2021-03-02 03:35:01 +0100

    added navigation, routes, your result page with no functionality yet

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,9 +1,12 @@
+import 'package:bmi_calculator/screens/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'reusable_card.dart';
-import 'icon_content.dart';
-import 'constants.dart';
-import 'round_icon_button.dart';
+import '../components/reusable_card.dart';
+import '../components/icon_content.dart';
+import '../constants.dart';
+import '../components/round_icon_button.dart';
+import 'results_page.dart';
+import '../components/bottom_button.dart';
 
 enum Gender {
   male,
@@ -209,11 +212,16 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerColor,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          BottomButton(
+            buttonTitle: 'CALCULATE',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return ResultsPage();
+                }),
+              );
+            },
           ),
         ],
       ),
